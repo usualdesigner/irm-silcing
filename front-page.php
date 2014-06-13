584e4ac1e7281eb468dd063d7727758958987049
usualdesigner <usualdesigner@gmail.com>
2014-06-13 17:23:31 +0300

    oOo

--- front-page.php
+++ front-page.php
@@ -74,7 +74,7 @@
 </div>
 <?php endif; ?>
 
-<?php $faq_posts = query_posts(array('post_type' => 'faq','posts_per_page' => -1, 'order' => 'ASC', 'orderby' => 'date')); ?>
+<?php $faq_posts = query_posts(array('post_type' => 'faq','posts_per_page' => -1, 'order' => 'DESC', 'orderby' => 'date')); ?>
 <?php if($faq_posts): ?>
     <div class="container bottom-distance" id="home-faq-slider">
         <div class="row">
